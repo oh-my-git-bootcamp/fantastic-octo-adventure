96ca4e43259eb61ca49a920efd616e3dc6603d6b
Cleber Grings <cgrings@centrosoftware.com>
2023-01-25 15:21:48 +0100

    Imaggine

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.centrosoftware.com/sites/default/files/cs-logo-w-tec_0.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
